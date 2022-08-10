d8c8972e6359d642a4c50da38c7a0f50e43e16c4
trollic <cece.jzhu@gmail.com>
2022-08-11 06:21:12 +1000

    changes to dispaly GameProjects

---
+++ frontend/src/pages/GameProjects.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+
+import { Context } from "../Context";
+import Header from "../components/Header";
+import ProjectsNotice from "../components/ProjectsNotice";
+import ProjectCard from "../components/ProjectCard";
+
+const GameProjects = () => {
+  const { gameProjects } = useContext(Context);
+
+  return (
+    <>
+      <Header />
+      <div>
+        <ProjectsNotice />
+        <div
+          className="d-flex align-content-start flex-wrap"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          {gameProjects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GameProjects;
